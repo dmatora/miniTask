b275a919d2626e00bcf915fc4f1d1ca7190f458e
Dmitry Matora <dmitry.matora@gmail.com>
2023-12-12 20:14:35 +0600

    Implemented randomuser.me IonItem scrollable list with IonItemSliding removal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React, { useEffect, useState } from "react";
+import {
+  IonAvatar,
+  IonContent,
+  IonImg,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
+  IonPage,
+} from "@ionic/react";
+import "./Home.css";
+
+interface User {
+  name: { title: string; first: string; last: string };
+  picture: { thumbnail: string };
+  email: string;
+}
 
 const Home: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=100")
+      .then((response) => response.json())
+      .then((data) => setUsers(data.results));
+  }, []);
+
+  const handleDelete = (email: string) => {
+    setUsers(users.filter((user) => user.email !== email));
+  };
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <IonContent>
+        <IonList>
+          {users.map((user) => (
+            <IonItemSliding key={user.email}>
+              <IonItem>
+                <IonAvatar slot="start">
+                  <IonImg src={user.picture.thumbnail} />
+                </IonAvatar>
+                <IonLabel>
+                  <h2>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h2>
+                  <p>{user.email}</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption
+                  color="danger"
+                  onClick={() => handleDelete(user.email)}
+                >
+                  Delete
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          ))}
+        </IonList>
+      </IonContent>{" "}
     </IonPage>
   );
 };
